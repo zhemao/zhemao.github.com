537a3b702c1ff2e073dd014645d31824f97ff8bd
Zhehao Mao <zhehaom@amazon.com>
2013-06-28 15:34:48 -0700

    update newpost script

--- newpost.sh
+++ newpost.sh
@@ -2,7 +2,8 @@ if [ -n "$1" ]; then
 	FNAME="_posts/`date +%Y-%m-%d-$1.markdown`"
 	echo '---' > $FNAME
 	echo 'title:' >> $FNAME
-	echo 'layout: post' >> $FNAME
+	echo 'layout: default' >> $FNAME
+	echo 'category: blog' >> $FNAME
 	echo '---' >> $FNAME
 	$EDITOR $FNAME
 fi
